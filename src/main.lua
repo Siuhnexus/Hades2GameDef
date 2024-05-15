9772e6afd6846a60fd8bc61154b6ca502c7627aa
Andre Louis Issa <andrelouisissa@gmail.com>
2024-05-15 15:48:50 +1000

    extra usage tip

--- src/main.lua
+++ src/main.lua
@@ -4,6 +4,7 @@
 --[[
 	  This file is intentionally blank, this plugin does nothing.
 	  Use the lua language server in VSCode to interact with its definitions.
+	  Works best if you consolidate the files, use `merge_defs.py`, then delete the `Scripts` folder.
 	  
 	  Usage:
 		---@module 'SGG_Modding-Hades2GameDef-Globals'
